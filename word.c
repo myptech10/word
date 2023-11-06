bdef1d4d884db9b11660aa0f8be4f28960fc4b76
myptech10 <131800520+myptech10@users.noreply.github.com>
2023-11-06 16:41:18 -0500

    Update word.c

--- word.c
+++ word.c
@@ -7,22 +7,18 @@
 
 #define MAX_LENGTH 100
 
-
-struct wordCounts {
-    char word[100];  
-    int count;
-};
-
 int main() {
 
 
 char buffer[1024];
+int fd;
 ssize_t bytes_read;  // number of characters/bytes read 
 int word_count = 0;
-int store_word[1000]; //store words in array
-int unique_word_count = 0;
-int fd;
-struct wordCounts word_counts[MAX_LENGTH];
+char store_word[100]; //store words in array
+int word_length = 0;
+
+
+
 
 
 
@@ -33,20 +29,23 @@ fd = open("sentence.txt", O_RDONLY);
     }
 
 while ((bytes_read = read(fd, buffer, sizeof(buffer)) > 0)) {
-        for (ssize_t i = 0; i < bytes_read; i++) {
+        for (ssize_t i = 0; i < bytes_read; i++) { //for each word 
+        
             char c = buffer[i];
 
-            if (word_length >= MAX_LENGTH || c == ' ' || !isalnum(c)) {
+        if (c == ' ' || !isalnum(c)) {
                 if (word_length > 0) {
                     // Null-terminate the word
                     store_word[word_length] = '\0';
-
-
+                    
                 }
-
+                else{
+                   
                 }
         }
 }
-
+        }
    return 0;
 }
+
+
